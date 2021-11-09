c79c1fd3b894942cd61a09228208a4919dfa77bf
unknown <gomolemosibiya@gmail.com>
2021-11-09 11:46:50 +0200

    send to back end

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -13,19 +13,34 @@ import {
 
 const InputBox = () => {
     
-  const [message, setMessage] = React.useState('')
+  const [message, setMessage] = useState('')
+
+  const onMicrophonePress = () => {
+    console.warn("Microphone pressed")
+  }
+
+  const onSendPress = async () => {
+     console.warn(`Sending: ${message}`);
+     //Send messge to backend
+     setMessage("");
+  }
+
+  const onPress = () => {
+    if (!message) {
+      onMicrophonePress();
+    } else {
+      onSendPress();
+    }
+  }
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={100}
-      style={{width: '100%'}}
-    >
+  
       <View style={styles.container}>
       <View style={styles.mainContainer}>
         <FontAwesome5 name="laugh-beam" size={24} color="grey" />
         <TextInput
           placeholder={"Type a message"}
-          style={styles.textInput}
+          style={styles.textInput} 
           multiline
           value={message}
           onChangeText={setMessage}
@@ -33,15 +48,14 @@ const InputBox = () => {
         <Entypo name="attachment" size={24} color="grey" style={styles.icon} />
         {!message && <Fontisto name="camera" size={24} color="grey" style={styles.icon} />}
       </View>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={onPress}>
         <View style={styles.buttonContainer}>
           {!message
-            ? <MaterialCommunityIcons name="microphone" size={28} color="white" />
+            ? <MaterialCommunityIcons  name="microphone" size={28} color="white" />
             : <MaterialIcons name="send" size={28} color="white" />}
         </View>
       </TouchableOpacity>
       </View>
-    </KeyboardAvoidingView>
   )
 }
 
